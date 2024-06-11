cc3b850ac4d01b07470123cf31a435b3e1ffa366
Jo Seung Hyo <seunghyo7742@naver.com>
2024-06-11 23:31:41 +0900

    Chore: CSS 리셋

--- Frontend/chat_app/src/styles/GlobalStyle.tsx
+++ Frontend/chat_app/src/styles/GlobalStyle.tsx
@@ -1,15 +1,28 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  *,*::before, *::after{
-  box-sizing: border-box
+  *, *::before, *::after {
+    box-sizing: border-box;
   }
-  body{
-  font-family: "Helvetica", "Arial", sans-serif;
-  line-height:1.5;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+  body {
+    font-family: "Helvetica", "Arial", sans-serif;
+    line-height: 1.5;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
+
+  ol, ul, menu {
+    list-style: none;
+  }
+
+  textarea {
+    white-space: revert;
+  }
+
+  input, textarea, select {
+    all: unset;
+    font-family: inherit; 
 `;
 export default GlobalStyle;
