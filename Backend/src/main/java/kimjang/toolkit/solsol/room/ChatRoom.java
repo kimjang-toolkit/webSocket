fa17aff47abdce0832d27214148e2243c07a188f
ohchansol <haxr369@gmail.com>
2024-06-12 01:01:23 +0900

    Feature/ 채팅, 채팅방, 사용자 레퍼지토리 인터페이스 작성

--- Backend/src/main/java/kimjang/toolkit/solsol/room/ChatRoom.java
+++ Backend/src/main/java/kimjang/toolkit/solsol/room/ChatRoom.java
@@ -2,9 +2,11 @@ package kimjang.toolkit.solsol.room;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Date;
+
 @RequiredArgsConstructor
 public class ChatRoom {
-    private final Long roomId;
-    private final String roomName;
-
+    private final Long id; // 방 번호
+    private final String roomName; // 방 이름
+    private Date createDate; // 언제 만들어졌는지
 }
