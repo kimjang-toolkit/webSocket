e5bdfcb61f4087b77da707094108cfd664086ff2
Jo Seung Hyo <seunghyo7742@naver.com>
2024-06-17 23:29:32 +0900

    Feat: ChatList component

--- Frontend/chat_app/src/components/ChatList.tsx
+++ Frontend/chat_app/src/components/ChatList.tsx
@@ -24,20 +24,45 @@ const MainContainer = styled.div`
   display: flex;
   width: 100%;
   height: 56px;
-  background: gray;
 `;
 const ProfileImg = styled.img`
   border-radius: 16px;
+  padding: 2px 2px;
 `;
 const InfoContainer = styled.div`
   display: flex;
-  flex-direction: cloumn;
+  flex-direction: column;
 `;
 const InfoWrapper = styled.div`
   display: flex;
   justify-content: space-between;
 `;
-const UserName = styled.p``;
-const RecentMessage = styled.div``;
-const TimeStamp = styled.p``;
-const CountBadge = styled.div``;
+const UserName = styled.p`
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 24px;
+`;
+const RecentMessage = styled.div`
+  color: #adb5bd;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 20px;
+`;
+const TimeStamp = styled.p`
+  font-size: 10px;
+  font-weight: 400;
+  line-height: 16px;
+  color: #a4a4a4;
+`;
+const CountBadge = styled.div`
+  display: flex;
+  align-items: center;
+
+  color: #001a83;
+  font-size: 10px;
+  font-weight: 600;
+  line-height: 16px;
+  border-radius: 40px;
+  background: #d2d5f9;
+`;
