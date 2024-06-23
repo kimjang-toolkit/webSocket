bb5caf551f0dfc392f07a894f26940d611f3be76
Jo Seung Hyo <seunghyo7742@naver.com>
2024-06-23 22:14:49 +0900

    Feat: Navbar publish

--- Frontend/chat_app/src/types/types.ts
+++ Frontend/chat_app/src/types/types.ts
@@ -12,12 +12,11 @@ export interface Customer {
   name: string;
 }
 
-//서버에서 보내는 데이터
+//서버에 보내는 데이터
 export interface MessageFormat {
   roomId: number;
   content: string;
   sender: Customer;
-  createDate: string;
 }
 //서버에서 받은 메시지데이터 가공
 export interface chatFormat {
