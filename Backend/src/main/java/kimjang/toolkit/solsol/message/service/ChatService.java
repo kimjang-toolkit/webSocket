3653db6ae8b7028fc8434f9bce0ba282f2dc3e91
ohchansol <haxr369@gmail.com>
2024-06-23 14:50:57 +0900

    ✨ Feat : 과거 채팅 내역 불러오기 서비스 작성
    To Do
    최근 채팅 10개 불러오기 BY 채팅 방 & 최근 퇴장 시간

--- Backend/src/main/java/kimjang/toolkit/solsol/message/service/ChatService.java
+++ Backend/src/main/java/kimjang/toolkit/solsol/message/service/ChatService.java
@@ -3,12 +3,16 @@ package kimjang.toolkit.solsol.message.service;
 import kimjang.toolkit.solsol.customer.Customer;
 import kimjang.toolkit.solsol.customer.CustomerRepository;
 import kimjang.toolkit.solsol.message.ChatMessage;
+import kimjang.toolkit.solsol.message.dto.PastChatsDto;
+import kimjang.toolkit.solsol.message.dto.ReqPastChatsDto;
 import kimjang.toolkit.solsol.message.dto.SendChatMessageDto;
 import kimjang.toolkit.solsol.message.repository.ChatRepository;
 import kimjang.toolkit.solsol.room.entity.ChatRoom;
 import kimjang.toolkit.solsol.room.repository.ChatRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +31,14 @@ public class ChatService {
     }
 
 
-
+    public PastChatsDto getPastChats(ReqPastChatsDto reqPastChatsDto) {
+        List<SendChatMessageDto> pastChats = chatRepository.findPastChats(reqPastChatsDto.getRoomId(),
+                reqPastChatsDto.getPageable());
+        return PastChatsDto.builder()
+                .roomId(reqPastChatsDto.getRoomId())
+                .pastChats(pastChats)
+                .page(reqPastChatsDto.getPageable().getPageNumber())
+                .size(reqPastChatsDto.getPageable().getPageSize())
+                .build();
+    }
 }
