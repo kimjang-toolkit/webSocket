44334bd5e8af41f3bd11a69692a8daa8ddfce36e
ohchansol <haxr369@gmail.com>
2024-06-24 01:35:10 +0900

    Feat/채팅 리스트 서비스 로직 작성

--- Backend/src/test/java/kimjang/toolkit/solsol/room/ChatRoomListTest.java
+++ Backend/src/test/java/kimjang/toolkit/solsol/room/ChatRoomListTest.java
@@ -15,14 +15,14 @@ public class ChatRoomListTest {
     @Autowired
     private ChatRoomService chatRoomService;
 
-//    @Test
-//    @DisplayName("특정 유저의 채팅방 리스트 불러오기")
-//    public void Get_User_Chat_Room_list(){
-//        List<ChatRoomDto> chatRoomDtoList = chatRoomService.getChatRooms(1L);
-//        chatRoomDtoList.stream().forEach(
-//                chatRoomDto -> {
-//                    System.out.println(chatRoomDto.getRoomName()+" "+chatRoomDto.getLastContent()+" "+chatRoomDto.getLastChatTime());
-//                }
-//        );
-//    }
+    @Test
+    @DisplayName("특정 유저의 채팅방 리스트 불러오기")
+    public void Get_User_Chat_Room_list(){
+        List<ChatRoomDto> chatRoomDtoList = chatRoomService.getChatRooms(1L);
+        chatRoomDtoList.stream().forEach(
+                chatRoomDto -> {
+                    System.out.println(chatRoomDto.getRoomName()+" "+chatRoomDto.getLastContent()+" "+chatRoomDto.getLastChatTime());
+                }
+        );
+    }
 }
