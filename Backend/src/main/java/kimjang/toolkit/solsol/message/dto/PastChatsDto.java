cdde824ef1e62b3adbe7b34b97564e234a461f6a
ohchansol <haxr369@gmail.com>
2024-06-24 18:22:33 +0900

    Fix/지난 채팅 기본 값 적용

--- Backend/src/main/java/kimjang/toolkit/solsol/message/dto/PastChatsDto.java
+++ Backend/src/main/java/kimjang/toolkit/solsol/message/dto/PastChatsDto.java
@@ -17,13 +17,15 @@ public class PastChatsDto {
     private List<SendChatMessageDto> pastChats;
     private int page; // 현재 페이지 번호
     private int size; // 페이지에 채팅 개수
+    private boolean hasNext;
 
-    public static PastChatsDto of(ReqPastChatsDto reqPastChatsDto, List<SendChatMessageDto> pastChats){
+    public static PastChatsDto of(ReqPastChatsDto reqPastChatsDto, Slice<SendChatMessageDto> pastChats){
         return PastChatsDto.builder()
                 .roomId(reqPastChatsDto.getRoomId())
-                .pastChats(pastChats)
+                .pastChats(pastChats.getContent())
                 .page(reqPastChatsDto.getPage().getPageNumber())
                 .size(reqPastChatsDto.getPage().getPageSize())
+                .hasNext(pastChats.hasNext())
                 .build();
     }
 }
