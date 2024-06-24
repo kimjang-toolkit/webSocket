a8f4beb0e1d6f2368b9857dda3e39340bc44de06
ohchansol <haxr369@gmail.com>
2024-06-24 13:00:53 +0900

    Fix/ChatRoom 응답 시 마지막 채팅 시간 포멧 수정

--- Backend/src/main/java/kimjang/toolkit/solsol/room/dto/ChatRoomDto.java
+++ Backend/src/main/java/kimjang/toolkit/solsol/room/dto/ChatRoomDto.java
@@ -1,5 +1,6 @@
 package kimjang.toolkit.solsol.room.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,9 @@ public class ChatRoomDto {
     private Long roomId;
     private String roomName;
     private Integer memberCnt;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, // JSON <-> String 파싱
+            pattern = "yyyy-MM-dd HH:mm:ss",
+            timezone = "Asia/Seoul")
     private LocalDateTime lastChatTime;
     private String lastContent;
 }
