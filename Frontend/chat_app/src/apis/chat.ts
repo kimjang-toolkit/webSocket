32b3bc247df2fac5d76630d73d31caed37770a98
Jo Seung Hyo <seunghyo7742@naver.com>
2024-06-25 00:54:39 +0900

    Feat: 이전 채팅 데이터 띄우기 성공

--- Frontend/chat_app/src/apis/chat.ts
+++ Frontend/chat_app/src/apis/chat.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-export const fetchChatHistory = async ({ pageParam = 0, queryKey }: { pageParam?: number, queryKey: any }) => {
+export const fetchChatHistory = async ({ pageParam = 0, queryKey }: { pageParam?: number; queryKey: any }) => {
   const [roomId, timeLine] = queryKey;
-  const { data } = await axios.get(`/chat-room/chat/${roomId}`, {
+  const { data } = await axios.get(`${import.meta.env.VITE_SPRING_URL}/chat-room/chat/${roomId}`, {
     params: {
       page: pageParam,
       timeLine,
+      roomExitTime: '2024-06-24 15:52:44',
+      size: 5,
     },
   });
   return data;
