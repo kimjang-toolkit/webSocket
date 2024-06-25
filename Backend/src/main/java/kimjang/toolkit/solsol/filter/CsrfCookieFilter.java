3f8a03fcd1e36eed575adbbb0843528accf3caa5
ohchansol <haxr369@gmail.com>
2024-06-25 18:57:21 +0900

    Feat : post 요청 인증 에러나는 부분 수정

--- Backend/src/main/java/kimjang/toolkit/solsol/filter/CsrfCookieFilter.java
+++ Backend/src/main/java/kimjang/toolkit/solsol/filter/CsrfCookieFilter.java
@@ -13,9 +13,12 @@ public class CsrfCookieFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         // 이용 가능한 Csrf 토큰을 찾는다.
+        System.out.println("헤더 이름 : "+CsrfToken.class.getName());
         CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+//        System.out.println(.);
+        System.out.println("CSRF 토큰 받았음 : "+ csrfToken.getToken() +" 이름 : "+csrfToken.getHeaderName());
         if(csrfToken != null){
-            System.out.println("CSRF 토큰 받았음 : "+ csrfToken.getToken());
+//            System.out.println("CSRF 토큰 받았음 : "+ csrfToken.getToken());
             response.setHeader(csrfToken.getHeaderName(), csrfToken.getToken());
         }
         filterChain.doFilter(request, response);
