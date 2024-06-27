4dae3d187124804995786bc61c9deba720188864
Jo Seung Hyo <seunghyo7742@naver.com>
2024-06-27 13:21:05 +0900

    Feat: 로그인페이지 구현완료

--- Frontend/chat_app/src/App.tsx
+++ Frontend/chat_app/src/App.tsx
@@ -1,12 +1,9 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import GlobalStyle from '@styles/GlobalStyle';
 import ChatRoomPage from '@/pages/ChatRoomPage';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '@/redux/store';
-import { useEffect } from 'react';
-import { initializeWebSocket } from '@/redux/webSocketSlice';
+
 import ChatListPage from '@/pages/ChatRoomListPage';
-import { setUser } from '@/redux/userSlice';
+import LoginPage from '@/pages/LoginPage';
 
 const router = createBrowserRouter([
   {
@@ -14,18 +11,9 @@ const router = createBrowserRouter([
     element: <ChatListPage />,
   },
   { path: '/chat', element: <ChatRoomPage /> },
+  { path: '/login', element: <LoginPage /> },
 ]);
 function App() {
-  const dispatch = useDispatch<AppDispatch>();
-
-  useEffect(() => {
-    dispatch(initializeWebSocket());
-    //임시 테스트를 위해 로컬스토리지로
-    const name = localStorage.getItem('userName');
-    const id = Number(localStorage.getItem('userId'));
-    dispatch(setUser({ name, id }));
-  }, [dispatch]);
-
   return (
     <>
       <GlobalStyle />
