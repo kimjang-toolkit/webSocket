d96919e840895da1fc74520d451491f04985ca24
ohchansol <haxr369@gmail.com>
2024-06-28 14:41:05 +0900

    Feat : Unsubscribe 요청을 보냈을 때 시간 저장 성공

--- Backend/src/main/java/kimjang/toolkit/solsol/room/repository/ChatRoomCustormerRelationshipRepository.java
+++ Backend/src/main/java/kimjang/toolkit/solsol/room/repository/ChatRoomCustormerRelationshipRepository.java
@@ -4,8 +4,11 @@ import kimjang.toolkit.solsol.room.entity.ChatRoomCustomerRelationship;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatRoomCustormerRelationshipRepository extends JpaRepository<ChatRoomCustomerRelationship, Long> {
     List<ChatRoomCustomerRelationship> findByChatRoom_Id(Long roomId);
     void deleteByChatRoom_IdAndUser_Email(Long roomId, String email);
+
+    Optional<ChatRoomCustomerRelationship> findByChatRoom_IdAndUser_Email(Long roomId, String email);
 }
