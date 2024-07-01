cb22e61a85f582e3a0120bb42d5738e196bcd719
ohchansol <haxr369@gmail.com>
2024-07-02 00:47:15 +0900

    Feat : 유저 프로파일 요청 기능 추가

---
+++ Backend/src/test/java/kimjang/toolkit/solsol/user/UniqueEmailTest.java
@@ -0,0 +1,24 @@
+package kimjang.toolkit.solsol.user;
+
+import kimjang.toolkit.solsol.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UniqueEmailTest {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    @DisplayName(value = "유저 이메일이 유니크한지 체크")
+    public void Check_Unique_Email(){
+        String email = "solsol@naver.com";
+        boolean check = userService.checkUniqueEmail(email);
+
+        Assertions.assertTrue(check);
+    }
+}
