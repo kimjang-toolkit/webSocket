d638e9f0a15f4fe2a6a72ab05ac080412e1e811d
ohchansol <haxr369@gmail.com>
2024-07-02 00:05:27 +0900

    Fix : 채팅 메세지가 유저의 최근 나간 시간을 기준으로 분리해서 전달하기

--- Backend/src/main/java/kimjang/toolkit/solsol/message/controllor/ChatController.java
+++ Backend/src/main/java/kimjang/toolkit/solsol/message/controllor/ChatController.java
@@ -23,8 +23,10 @@ public class ChatController {
      * 채팅방 나간 시간 이전, 이후에 생성된 채팅을 불러오는 API
      * timeLine이 past라면 채팅방 나간 시간 이전에 생성된 채팅
      * timeLine이 recent라면 채팅방 나간 시간 이후에 생성된 채팅
+     *
+     * 이미 유저와 채팅방 연관 관계에 최근 나간 시간을 저장하기 때문에 프론트에게 받을 필요가 없다.
+     *
      * @param roomId
-     * @param roomExitTime
      * @param page
      * @param size
      * @param timeLine
@@ -32,11 +34,12 @@ public class ChatController {
      */
     @GetMapping("/chat-room/chat/{roomId}")
     public ResponseEntity<PastChatsDto> getChatLogs(@PathVariable Long roomId,
-                                                    @RequestParam(defaultValue = "0001-01-01 00:00:00") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime roomExitTime,
+//                                                    @RequestParam(defaultValue = "0001-01-01 00:00:00") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime roomExitTime,
+                                                    @RequestParam Long userId,
                                                     @RequestParam(defaultValue = "0") Integer page,
                                                     @RequestParam(defaultValue = "100") Integer size,
                                                     @RequestParam(defaultValue = "recent") String timeLine){
-        ReqPastChatsDto reqPastChatsDto = new ReqPastChatsDto(roomId, page, size, roomExitTime);
+        ReqPastChatsDto reqPastChatsDto = new ReqPastChatsDto(roomId, userId, page, size);
         PastChatsDto pastChatsDto = chatService.getPastChats(reqPastChatsDto, timeLine);
         return ResponseEntity.ok(pastChatsDto);
     }
