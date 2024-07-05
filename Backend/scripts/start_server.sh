745a75a45444f3273fc3415637443592b17cd961
ohchansol <haxr369@gmail.com>
2024-07-06 02:35:19 +0900

    Fix : start_server 수정

--- Backend/scripts/start_server.sh
+++ Backend/scripts/start_server.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# Spring Boot 애플리케이션을 백그라운드로 실행
-java -Dserver.port=80 -jar -Dspring.profiles.active=prod /home/ec2-user/server/app.jar > /dev/null 2> /dev/null < /dev/null &
+echo "빌드 버전 2.0.0" >> /home/ec2-user/server/deploy.log
+
+BUILD_JAR=$(ls /home/ec2-user/server/*.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo ">>> build 파일명: $JAR_NAME" >> /home/ec2-user/server/deploy.log
+
+echo ">>> build 파일 복사" >> /home/ec2-user/server/deploy.log
+
+DEPLOY_JAR=/home/ec2-user/server/$JAR_NAME
+
+echo ">>> DEPLOY_JAR 배포"    >> /home/ec2-user/server/deploy.log
+nohup java -Dserver.port=80 -jar -Dspring.profiles.active=prod /home/ec2-user/server/app.jar >> /home/ec2-user/deploy.log 2>/home/ec2-user/server/deploy_err.log &
 
 # 백그라운드 실행 확인 메시지
 echo "Spring Boot 애플리케이션이 백그라운드에서 실행 중입니다."
