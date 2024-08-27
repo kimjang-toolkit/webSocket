731d308380002975f815e476b962c96d8759debc
ohchansol <haxr369@gmail.com>
2024-08-27 21:59:08 +0900

    fix: 구독 취소 예외 메시지 수정

--- Backend/src/main/java/kimjang/toolkit/solsol/config/container/WSSessionRoomContainer.java
+++ Backend/src/main/java/kimjang/toolkit/solsol/config/container/WSSessionRoomContainer.java
@@ -10,19 +10,6 @@ public class WSSessionRoomContainer implements SocketSessionContainer{
     private final Map<String, Long> roomContainer = new ConcurrentHashMap<>();
     private final Map<String, String> emailContainer = new ConcurrentHashMap<>();
 
-
-    @Override
-    public Long getSubscribeUri(String session) {
-        return roomContainer.getOrDefault(session, null);
-    }
-
-    @Override
-    public Long unsubscribe(String session) {
-        if(!roomContainer.containsKey(session))
-            throw new IllegalStateException("구독 중이지 않습니다.");
-        return roomContainer.remove(session);
-    }
-
     @Override
     public void subscribe(String session, Long roomId) {
         roomContainer.put(session, roomId);
@@ -34,16 +21,25 @@ public class WSSessionRoomContainer implements SocketSessionContainer{
     }
 
     @Override
-    public String getEmail(String session) {
-        return emailContainer.getOrDefault(session, null);
+    public Long unsubscribe(String session) {
+        if(!roomContainer.containsKey(session))
+            throw new IllegalStateException("세션이 room을 구독 중이지 않습니다.");
+        return roomContainer.remove(session);
     }
 
     @Override
     public String unsetEmail(String session) {
         if(!emailContainer.containsKey(session))
-            throw new IllegalStateException("구독 중이지 않습니다.");
+            throw new IllegalStateException("세션이 email을 가지고 있지 않습니다.");
         return emailContainer.remove(session);
     }
 
-
+    @Override
+    public Long getSubscribeUri(String session) {
+        return roomContainer.getOrDefault(session, null);
+    }
+    @Override
+    public String getEmail(String session) {
+        return emailContainer.getOrDefault(session, null);
+    }
 }
